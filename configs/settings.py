57df794f5521cccc8d346a83d9e043d3db168162
Vacheslav <svm@familyc.ru>
2018-09-18 11:04:30 +0500

    Fix
    Изменение зависимостей для установки

--- configs/settings.py
+++ configs/settings.py
@@ -100,27 +100,23 @@ WSGI_APPLICATION = 'sopds.wsgi.application'
 #    }             
 #}
 
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'sopds',
+    'USER': 'sopds',
+    'PASSWORD': 'sopds',
+    'HOST': '', # Set to empty string for localhost.
+    'PORT': '', # Set to empty string for default.
+    }
+}
+
 #DATABASES = {
 #    'default': {
-#    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#    'NAME': 'sopds',
-#    'USER': 'sopds',
-#    'PASSWORD': 'sopds',
-#    'HOST': '', # Set to empty string for localhost.
-#    'PORT': '', # Set to empty string for default.
-#    }
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }         
 #}
-
-DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': os.environ["DB_NAME"],
-    'USER': os.environ["DB_USER"],
-    'PASSWORD': os.environ["DB_PASS"],
-    'HOST': os.environ["DB_HOST"], # Set to empty string for localhost.
-    'PORT': os.environ["DB_PORT"], # Set to empty string for default.
-    }
-}
 
 #### SOPDS DATABASE SETTINGS FINISH ####
 
@@ -149,7 +145,7 @@ LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'sopds/locale'),
 )
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'Asia/Yekaterinburg'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
